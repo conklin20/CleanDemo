fa154ea9f3e728b3e0e2bc555f0c4d3ce519068a
Cary Conklin <cary.conklin.20@gmail.com>
2022-08-10 18:57:21 -0500

    End section 6

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 
@@ -6,30 +6,53 @@ interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
+export default function ActivityList({ activities, selectActivity, deleteActivity, submitting }: Props) {
+    const [target , setTarget] = React.useState('');
+
+    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+        // e.stopPropagation();
+        setTarget(e.currentTarget.name);
+        deleteActivity(id);
+    }
+
     return (
-        <Segment>
-            <Item.Group divided>
-                {activities.map(activity => (
-                    <Item key={activity.id}>
-                        <Item.Content>
-                            <Item.Header as='a'>{activity.title}</Item.Header>
-                            <Item.Meta>{activity.date}</Item.Meta>
-                            <Item.Description>
-                                <div>{activity.description}</div>
-                                <div>{activity.city}, {activity.venue}</div>
-                            </Item.Description>
-                            <Item.Extra>
-                                <Button onClick={() => selectActivity(activity.id)} floated='right' content='View' color='blue' />
-                                <Button onClick={() => deleteActivity(activity.id)} floated='right' content='Delete' color='red' />
-                                <Label basic content={activity.category} />
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>
-                ))}
-            </Item.Group>
-        </Segment>
-    )
+      <Segment>
+        <Item.Group divided>
+          {activities.map(activity => (
+            <Item key={activity.id}>
+              <Item.Content>
+                <Item.Header as="a">{activity.title}</Item.Header>
+                <Item.Meta>{activity.date}</Item.Meta>
+                <Item.Description>
+                  <div>{activity.description}</div>
+                  <div>
+                    {activity.city}, {activity.venue}
+                  </div>
+                </Item.Description>
+                <Item.Extra>
+                  <Button
+                    onClick={() => selectActivity(activity.id)}
+                    floated="right"
+                    content="View"
+                    color="blue"
+                  />
+                  <Button
+                    name={activity.id}
+                    loading={submitting && target === activity.id}
+                    onClick={e => handleActivityDelete(e, activity.id)}
+                    floated="right"
+                    content="Delete"
+                    color="red"
+                  />
+                  <Label basic content={activity.category} />
+                </Item.Extra>
+              </Item.Content>
+            </Item>
+          ))}
+        </Item.Group>
+      </Segment>
+    );
 }
